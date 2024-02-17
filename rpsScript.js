2131a4d4523a58b39c5d731d9e47bea890349cc4
Vijay2887 <145547677+Vijay2887@users.noreply.github.com>
2024-02-17 18:25:00 +0530

    Add files via upload

---
+++ rpsScript.js
@@ -0,0 +1,31 @@
+const playerChoice = document.getElementById("playerChoice");
+const computerChoice = document.getElementById("computerChoice");
+const resultLine = document.getElementById("resultLine");
+
+const choices = ['rock', 'paper', 'scissor'];
+
+
+function playGame(choice)
+{
+    let result = "";
+    let computerChooses = choices[Math.floor(Math.random()*3)];
+    if(choice == computerChooses){
+        result = "IT's A TIE😐";
+    }
+
+    else{
+        if(choice == 'rock') {
+            result = (computerChooses == 'paper' ) ? "YOU LOSE😥" : "YOU WIN😎";
+        }
+        else if(choice == 'paper') {
+            result = (computerChooses == 'scissor' ) ? "YOU LOSE😥" : "YOU WIN😎";
+        }
+        else if(choice == 'scissor') {
+            result = (computerChooses == 'rock' ) ? "YOU LOSE😥" : "YOU WIN😎";
+        }
+    }
+
+    playerChoice.textContent = "Player's Choice:" + `${choice}`;
+    computerChoice.textContent = "Computer's Choice:" + `${computerChooses}`;
+    resultLine.textContent = `${result}`;
+}
